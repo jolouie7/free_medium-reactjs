6c5087f4077b0c8457f09af412c0e526351a78a0
Your_Name <joseph161216@gmail.com>
2020-11-18 19:40:35 -0800

    update backend host

--- src/constants/api-config.ts
+++ src/constants/api-config.ts
@@ -3,12 +3,13 @@ let backendHost: string;
 
 const hostname = window && window.location && window.location.hostname;
 
-if (hostname === "free-medium-mern.herokuapp.com") {
+if (hostname === "free-medium-reactjs.vercel.app/") {
   backendHost = "https://free-medium-mern.herokuapp.com";
 } else if (/^qa/.test(hostname)) {
   backendHost = `https://api.${hostname}`;
 } else {
-  backendHost = process.env.REACT_APP_BACKEND_HOST || "http://localhost:5000";
+  backendHost =
+    "https://free-medium-mern.herokuapp.com" || "http://localhost:5000";
 }
 
 export default backendHost;
